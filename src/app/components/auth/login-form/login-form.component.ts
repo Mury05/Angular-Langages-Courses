7498187cbde2784e74f555a2844c56ac4ffbba29
Mury05 <murielleodilone@gmail.com>
2024-12-19 10:49:27 +0100

    Logique de connexion debut

--- src/app/components/auth/login-form/login-form.component.ts
+++ src/app/components/auth/login-form/login-form.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   standalone: false,
-  
+
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
-
+  onSubmit(data: NgForm) {
+    console.log('Form submitted', data);
+  }
 }
---
+++ src/app/data/userConnexion.ts
@@ -0,0 +1,7 @@
+import { UserConnexion } from "../models/user-connexion";
+
+export const  USER: UserConnexion[] = [
+  {id: 0, email: "mailto:test@gmail.com", password: "password1234"},
+  {id: 1, email: "mailto:johndoe@gmail.com", password: "password456"},
+  {id: 2, email: "mailto:janedoe@gmail.com", password: "password789"},
+]
